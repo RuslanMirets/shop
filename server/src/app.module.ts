816213967e65f0360135943d77dadc4438c7e113
Ruslan Mirets <ruslan.mirets@gmail.com>
2022-02-14 10:13:36 +0300

    Создание CRUD для пользователей

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,7 +1,22 @@
+import { UserEntity } from './user/entities/user.entity';
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '123456',
+      database: 'shop',
+      entities: [UserEntity],
+      synchronize: true,
+    }),
+    UserModule,
+  ],
   controllers: [],
   providers: [],
 })
