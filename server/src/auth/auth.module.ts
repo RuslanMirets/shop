9c3cbcf9a1c65ad7bf109e985b5cb4e812931e5f
Ruslan <ruslan.mirets@gmail.com>
2022-02-14 12:04:42 +0300

    Авторизация с помощью PassportJS (логин + пароль)

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -1,11 +1,13 @@
+import { LocalStrategy } from './strategies/local.strategy';
 import { UserModule } from './../user/user.module';
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, PassportModule],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, LocalStrategy],
 })
 export class AuthModule {}
